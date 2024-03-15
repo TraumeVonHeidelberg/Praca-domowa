f17711cf03b674b6e810271593b20e9ff2a06dff
Träume von Heidelberg <jsd.usis@gmail.com>
2024-03-15 15:34:11 +0100

    Added function to calculate a number factorial

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,37 @@
 
 using namespace std;
 
+#include <iostream>
+
+using namespace std;
+
+int factiorial(int a)
+{
+    if(a==0)
+    {
+        return 1;
+    }
+
+    return a* factiorial(a-1);
+}
+
+int main() {
+    int a, b;
+    cout << "Podaj liczbe calkowita ";
+    cin >> a >> b;
+    int wyjscie;
+    do {
+        cout << endl;
+        cout << endl;
+        cout << "MENU" << endl;
+        cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
+        cout << "0. Wyjscie" << endl;
+        cin >> wyjscie;
+    } while(wyjscie != 0);
+    return 0;
+}
+
+
 int main() {
     int a, b;
     cout << "Podaj liczbe calkowita ";
