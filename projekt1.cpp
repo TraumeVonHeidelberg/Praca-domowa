37b92d85b0c53a360471a0419fc3475d3c867f35
Träume von Heidelberg <jsd.usis@gmail.com>
2024-03-15 15:46:31 +0100

    Added menu option to check if the number is prime

--- projekt1.cpp
+++ projekt1.cpp
@@ -24,17 +24,22 @@ bool isPrimary(int a)
 }
 
 int main() {
-    int a, b;
-    cout << "Podaj liczbe calkowita ";
-    cin >> a >> b;
+    int a;
     int wyjscie;
     do {
-        cout << endl;
-        cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
         cout << "0. Wyjscie" << endl;
+        cout << "2. Czy pierwsza" << endl;
         cin >> wyjscie;
+
+        if(wyjscie==2)
+        {
+            cout<<"Wpisz liczbe by sprawdzic czy jest pierwsza: ";
+            cin>>a;
+
+            isPrimary(a)?cout<<"Pierwsza"<<endl:cout<<"Zlozona"<<endl;
+        }
     } while(wyjscie != 0);
     return 0;
 }
